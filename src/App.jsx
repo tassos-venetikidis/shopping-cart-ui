478fb64ca718891012ebf240ea25c8d069b14097
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-02-10 22:41:45 +0200

    Added a proxy for /api to the development-only JSON server

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   useEffect(() => {
     async function getProducts() {
       try {
-        const res = await fetch("http://localhost:8000/products");
+        const res = await fetch("/api/products");
         if (!res.ok) throw new Error("Failed to fetch products...");
         const data = await res.json();
         setProducts(data);
